8b7b2cf41bccd6e336fea2b06de3bdc9f89d4a44
Maxim Koylo <lkoylol12@gmail.com>
2020-12-28 17:06:17 +0200

    create players cards

--- client/src/game/players/players.tsx
+++ client/src/game/players/players.tsx
@@ -1,11 +1,29 @@
+import { observer } from 'mobx-react';
+
+import styled from 'styled-components/macro';
+
 import { playersStore } from '../../common/store/players-store';
 
-export const Players = () => {
+const Wrapper = styled.div`
+  padding-top: 1rem;
+`;
+
+const PlayerCard = styled.div`
+  margin-bottom: 1.5rem;
+  padding: 1rem;
+  color: #fff;
+  background-color: #161b1b;
+`;
+
+export const Players = observer(() => {
   return (
-    <div>
+    <Wrapper>
       {playersStore.players.map((player) => (
-        <div>{player.name}</div>
+        <PlayerCard>
+          <div>{player.name}</div>
+          <div>{15000}$</div>
+        </PlayerCard>
       ))}
-    </div>
+    </Wrapper>
   );
-};
+});
